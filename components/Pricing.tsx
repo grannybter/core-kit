8fdb70abf323748eea8543be99decd023a39379f
tommydanna <tommydanna@gmail.com>
2023-02-27 09:22:14 -0700

    docs

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -50,7 +50,7 @@ export default function Pricing({ products }: Props) {
       <section className="bg-mainwhite">
         <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
           <div className="sm:flex sm:flex-col sm:align-center"></div>
-          <p className="text-6xl font-extrabold text-white sm:text-center sm:text-6xl">
+          <p className="text-6xl font-extrabold text-black sm:text-center sm:text-6xl">
             No subscription pricing plans found. Create them in your{' '}
             <a
               className="text-azul underline"
