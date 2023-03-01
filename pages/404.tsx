4d7e144ac885a4f57c087d3d2b111da988a1357a
tommydanna <tommydanna@gmail.com>
2023-03-01 10:50:55 -0700

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,21 +12,21 @@ export default function Four0Four() {
           <body class="h-full">
           ```
         */}
-        <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
+        <main className="grid min-h-screen place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
           <div className="text-center">
             <p className="text-base font-semibold text-azul">404</p>
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
             <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link href="/dashboard">
+            <Link href="/">
                   <a className="group inline-flex items-center rounded-full bg-slate-900 px-4 py-1.5 font-semibold text-white transition hover:bg-slate-700">
                     Go back home
                     <Arrow />
                   </a>
                 </Link>
 
-                <Link href="/dashboard">
-                  <a className="group inline-flex items-center rounded-full bg-transparent px-4 py-1.5 font-semibold text-slate-700 transition hover:bg-gray-100">
+                <Link href="https://github.com/grannybter/core-kit/issues">
+                  <a target="_blank" className="group inline-flex items-center rounded-full bg-transparent px-4 py-1.5 font-semibold text-slate-700 transition hover:bg-gray-100">
                     Contact support
                     <Arrow stroke="black"/>
                   </a>
