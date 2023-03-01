2b1bf5f5fb648eba425193fb5a02e6d8e4ed3b6a
tommydanna <tommydanna@gmail.com>
2023-02-28 19:58:07 -0700

    removed recent commit from demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Pricing from 'components/Pricing';
 import { Product } from 'types';
 import Hero from 'components/ui/Hero';
-import Footer from 'components/ui/Footer';
 import Features from 'components/ui/Features';
 import Features2 from 'components/ui/Features2';
 import CTA from 'components/ui/CTA';
-import LastCommit from 'components/ui/LastCommit';
 import LogoCloud from 'components/ui/LogoCloud';
 
 export default function HomePage() {
@@ -14,7 +12,6 @@ export default function HomePage() {
       <Hero/> 
       <LogoCloud/>
       <Features/>
-      <LastCommit/>
       <Features2/>
       <CTA/>
     </>
